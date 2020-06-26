8074f3a85fd408e7a35a353eaaaf1e65f0d03496
Biking0 <1342831695@qq.com>
2020-06-26 10:05:17 +0800

    pagenum

--- hive_to_hive/copy_hdfs_data/test_distcp/test.sh
+++ hive_to_hive/copy_hdfs_data/test_distcp/test.sh
@@ -1,7 +1,7 @@
 # 测试distcp参数
 
--pb
--
+
+
 
 
 测试环境
@@ -18,4 +18,8 @@ hdfs://192.168.190.88:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_white
 hadoop fs -du -h hdfs://192.168.190.89:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_whitelist_day/
 hadoop fs -du -h hdfs://192.168.190.89:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_whitelist_day/statis_date=20170617
 
+hadoop distcp -append -update -bandwidth 15 -m 50 -diff -i -log ./ hdfs://192.168.190.89:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_whitelist_day/statis_date=20170617 hdfs://172.19.168.4:8020/warehouse/tablespace/managed/hive/tb_si_cu_voma_limit_whitelist_day
 
+hadoop fs -du -h hdfs://192.168.190.89:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_whitelist_day | more
+
+hadoop fs -du -h hdfs://192.168.190.89:8020/apps/hive/warehouse/csap.db/tb_si_cu_voma_limit_whitelist_day/statis_date=20170617
